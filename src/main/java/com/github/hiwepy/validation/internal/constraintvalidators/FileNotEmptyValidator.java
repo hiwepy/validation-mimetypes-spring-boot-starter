9907a4e5827f50327b963650f120a8002fd66b9c
wandl <hiwepy@163.com>
2022-11-07 18:17:02 +0800

    文件校验优化，FileNotEmpty

---
+++ src/main/java/com/github/hiwepy/validation/internal/constraintvalidators/FileNotEmptyValidator.java
@@ -0,0 +1,90 @@
+package com.github.hiwepy.validation.internal.constraintvalidators;
+
+import com.github.hiwepy.validation.MimeTypeDetectorHolder;
+import com.github.hiwepy.validation.constraints.FileNotEmpty;
+import com.github.hiwepy.validation.utils.FilemimeUtils;
+import com.github.hiwepy.validation.utils.FiletypeUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.overviewproject.mime_types.GetBytesException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.util.StringUtils;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 单文件校验
+ * @author wandl
+ * @version 1.0
+ * @since 2022.11.07
+ */
+public class FileNotEmptyValidator implements ConstraintValidator<FileNotEmpty, MultipartFile> {
+
+    private Logger log = LoggerFactory.getLogger(FileNotEmptyValidator.class);
+    private Set<String> extensionSet = new HashSet<>();
+    private Set<String> mimeTypeSet = new HashSet<>();
+    private DataSize maxSize;
+    private boolean required;
+
+    @Override
+    public void initialize(FileNotEmpty annotation) {
+        this.extensionSet = ArrayUtils.isNotEmpty(annotation.extensions()) ? Stream.of(annotation.extensions()).map(ext -> ext.toLowerCase()).collect(Collectors.toSet()) : Collections.emptySet();
+        this.mimeTypeSet = ArrayUtils.isNotEmpty(annotation.mimeTypes()) ?  Stream.of(annotation.mimeTypes()).map(mime -> mime.toLowerCase()).collect(Collectors.toSet()) : Collections.emptySet();
+        this.required = annotation.required();
+        this.maxSize = StringUtils.hasText(annotation.maxSize()) ? DataSize.parse(annotation.maxSize(), DataUnit.BYTES): null;
+    }
+
+    @Override
+    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext constraintValidatorContext) {
+
+        // 1、验证文件是否为空
+        if (Objects.isNull(multipartFile) || multipartFile.isEmpty()) {
+            return !required;
+        }
+        // 2、验证文件大小是否满足要求
+        if (Objects.nonNull(maxSize)) {
+            return maxSize.compareTo(DataSize.of(multipartFile.getSize(), DataUnit.BYTES)) > 0;
+        }
+        // 3、验证文件后缀是否满足要求
+        if (!extensionSet.isEmpty()) {
+            String detectExtension = null;
+            try {
+                detectExtension = FiletypeUtils.getFileType(multipartFile.getBytes());
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+            if(!StringUtils.hasText(detectExtension)){
+                detectExtension = FilenameUtils.getExtension(multipartFile.getName());
+            }
+            if(!StringUtils.hasText(detectExtension)){
+               return Boolean.FALSE;
+            }
+            return extensionSet.contains(detectExtension.toLowerCase());
+        }
+        // 4、验证文件 content type 是否满足要求
+        if (!mimeTypeSet.isEmpty()) {
+            String detectMimeType = null;
+            try {
+                detectMimeType = MimeTypeDetectorHolder.instance().getDetector().detectMimeType(multipartFile.getName(), multipartFile.getInputStream());
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                return Boolean.FALSE;
+            }
+            if(StringUtils.hasText(detectMimeType)){
+                return mimeTypeSet.contains(detectMimeType.toLowerCase());
+            }
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+}
