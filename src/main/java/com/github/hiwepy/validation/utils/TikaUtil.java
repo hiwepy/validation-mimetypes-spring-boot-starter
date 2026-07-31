3ab63e34c1ad8aa2414d5e3b68131b499fb687da
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 22:23:02 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

--- src/main/java/com/github/hiwepy/validation/utils/TikaUtil.java
+++ src/main/java/com/github/hiwepy/validation/utils/TikaUtil.java
@@ -1,4 +1,4 @@
-package io.github.hiwepy.validation.utils;
+package io.github.easy4j.validation.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
