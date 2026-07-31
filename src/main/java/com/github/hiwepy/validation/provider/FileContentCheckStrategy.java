b67b19d695f313f39429b8aa946c9f2ee6e93392
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 22:23:02 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

--- src/main/java/com/github/hiwepy/validation/provider/FileContentCheckStrategy.java
+++ src/main/java/com/github/hiwepy/validation/provider/FileContentCheckStrategy.java
@@ -1,4 +1,4 @@
-package io.github.hiwepy.validation.provider;
+package io.github.easy4j.validation.provider;
 
 import org.apache.tika.mime.MimeType;
 import org.springframework.beans.factory.InitializingBean;
