63937ad92f486302823a15b1820e330fed7b157d
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:25:54 +0800

    docs: correct @author tag to [@Loong Wan](https://github.com/loong10k)
    - Replace incorrect 'wandl' / 'hiwepy' / 'easy-4-java contributors' attributions
      with the canonical author reference [@Loong Wan](https://github.com/loong10k)
    - Apply to both generated unit tests and existing source files
    - Consistent with project maintenance ownership

--- src/test/java/io/github/easy4j/validation/Test.java
+++ src/test/java/io/github/easy4j/validation/Test.java
@@ -26,7 +26,7 @@ import jakarta.activation.MimetypesFileTypeMap;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/easy-4-java">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 
 public class Test {
